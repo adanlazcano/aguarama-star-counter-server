7c2f85bc5d0f9dd718ebf40b098cc6857608bceb
adanlazcano <71905406+adanlazcano@users.noreply.github.com>
2021-08-05 15:28:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-import { enviroment } from './src/config/config.js';
+import { environment } from './src/config/config.js';
 import './src/config/database.js';
 import ClientRoute from './src/routes/Client.js';
 const app = express();
 
 //settings
-app.set('port', process.env.PORT || enviroment.port);
+app.set('port', process.env.PORT || environment.port);
 app.set('json spaces', 2);
 
 //Middlewares
